7e77d89a8220cd44ce00ce2ca8cc07e36dbf2dd0
Alec M <39228141+HeavenlyEntity@users.noreply.github.com>
2018-12-10 23:33:55 -0700

    OathMedia Single PageTemplate

---
+++ OathMedia Site 1/index.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+// Is this browser sufficiently modern to continue?
+if ( !( "querySelector" in document
+   && "addEventListener" in window
+   && "getComputedStyle" in window) ) {
+   return;
+}
+window.document.documentElement.className +=
+   " enhanced";
